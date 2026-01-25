19a0b138e868d72be075b0ef51198cc5d39333f3
rockman6258 <64748238+rockman6258@users.noreply.github.com>
2026-01-24 23:42:03 -0600

    Update motor and sensor ports in subsystems

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -7,15 +7,14 @@ extern Drive chassis;
 
 // Your motors, sensors, etc. should go here.  Below are examples
 
- inline pros::Motor topIntake(-19 );
+ inline pros::Motor topIntake(-20 );
 // inline pros::adi::DigitalIn limit_switch('A');
  inline ez::Piston wings('G'); 
  inline pros::Motor bottomIntake(7);
  inline ez::Piston scraper('H');
  inline pros::Motor middleIntake(-15);
- inline pros::MotorGroup leftMotors({ -2, 16, -12});
- inline pros::MotorGroup rightMotors({5, -14, 13} );
+ inline pros::MotorGroup leftMotors({ -16, 12, -2});
+ inline pros::MotorGroup rightMotors({13, -14, 11} );
  inline ez::Piston  doublePark('E');  
  inline pros ::Optical optical ( 8 );
  inline pros::Distance distancesensor(3);
-
