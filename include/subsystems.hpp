9a98d26eb89a990f8729782aebda37f28d1c0718
rockman6258 <64748238+rockman6258@users.noreply.github.com>
2026-02-01 19:15:18 -0600

    Modify subsystem motor and piston configurations
    Updated motor and piston assignments for subsystem configuration.

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -9,14 +9,12 @@ extern Drive chassis;
 
  inline pros::Motor topIntake(-8);
 // inline pros::adi::DigitalIn limit_switch('A');
- inline ez::Piston wings('G'); 
+ inline ez::Piston wings('H'); 
  inline pros::Motor bottomIntake(7);
- inline ez::Piston scraper('H');
+ inline ez::Piston scraper('G');
  inline pros::Motor middleIntake(-15);
- inline pros::MotorGroup leftMotors({ -16, 12, -2});
- inline pros::MotorGroup rightMotors({13, -14, 11} );
+ inline pros::MotorGroup leftMotors({ -6, 12, -2}); //changed from -16, 12, -2
+ inline pros::MotorGroup rightMotors({13, -14, 16} ); //changed from 13, -14, 11
  inline ez::Piston intakeLift('E');  
  inline pros ::Optical optical ( 8 );
  inline pros::Distance distancesensor(3);
-
- 
