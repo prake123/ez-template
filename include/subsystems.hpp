4532b14a2c71d0ff58137a7d6db7243a289a7921
rockman6258 <64748238+rockman6258@users.noreply.github.com>
2026-02-01 19:18:05 -0600

    Update motor and piston configurations in subsystems

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -7,14 +7,14 @@ extern Drive chassis;
 
 // Your motors, sensors, etc. should go here.  Below are examples
 
- inline pros::Motor topIntake(-8 );
+ inline pros::Motor topIntake(-8);
 // inline pros::adi::DigitalIn limit_switch('A');
- inline ez::Piston wings('G'); 
+ inline ez::Piston wings('H'); 
  inline pros::Motor bottomIntake(7);
- inline ez::Piston scraper('H');
+ inline ez::Piston scraper('G');
  inline pros::Motor middleIntake(-15);
- inline pros::MotorGroup leftMotors({ -16, 12, -2});
- inline pros::MotorGroup rightMotors({13, -14, 11} );
+ inline pros::MotorGroup leftMotors({ -6, 12, -2});
+ inline pros::MotorGroup rightMotors({13, -14, 16} );
  inline ez::Piston  doublePark('E');  
  inline pros ::Optical optical ( 8 );
  inline pros::Distance distancesensor(3);
