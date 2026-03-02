193bdc58cb25dd2d92b4ee089aab83045639f859
Darwin Yee <darwinyee@DMPro.local>
2026-03-02 12:17:34 -0600

    idk

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -9,20 +9,12 @@ extern Drive chassis;
 
  inline pros::Motor topIntake(-8);
 // inline pros::adi::DigitalIn limit_switch('A');
- inline ez::Piston wings('H'); 
+ inline ez::Piston wings('H');
  inline pros::Motor bottomIntake(7);
  inline ez::Piston scraper('G');
  inline pros::Motor middleIntake(-15);
-<<<<<<< HEAD
  inline pros::MotorGroup leftMotors({2, 11, 12}); // NOT USED
  inline pros::MotorGroup rightMotors({4, 13, 14}); // NOT USED
  inline ez::Piston doublePark('E');  
  inline pros::Optical optical(8);
  inline pros::Distance distancesensor(3);
-=======
- inline pros::MotorGroup leftMotors({ -2, 11, -12});
- inline pros::MotorGroup rightMotors({4, -14, 13} );
- inline ez::Piston  doublePark('E');  
- inline pros ::Optical optical ( 8 );
- inline pros::Distance distancesensor(3);
->>>>>>> origin/master
